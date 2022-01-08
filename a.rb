8ad1f7a7ffa708c05f0e45a3379f3b809c22bf38
tacker530 <tacker530i@gmail.com>
2022-01-08 21:08:22 +0900

    A AC!

--- a.rb
+++ a.rb
@@ -1,1 +1,14 @@
-a.rb
+# A - Weird Function
+#  f(f(f(t)+t)+f(f(t)))
+#  f(x)=x^2+2x+3
+# 
+t = gets.chomp.to_i
+
+STDERR.puts "x = #{t}"
+
+def f(x)
+  x**2 + 2*x + 3
+end
+
+y = f(f(f(t)+t)+f(f(t)))
+puts "#{y}"
