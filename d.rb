c1e968c83f87e9ca56f37adc87563e8ca1879a5f
tacker530 <tacker530i@gmail.com>
2022-01-08 22:39:33 +0900

    TLEとれない～

--- d.rb
+++ d.rb
@@ -4,7 +4,20 @@
 N,K = gets.chomp.split.map(&:to_i)
 P =   gets.chomp.split.map(&:to_i)
 
-STDERR.puts "[STDERR] Params : N=#{N} K=#{K} P=#{P}"
+#STDERR.puts "[STDERR] Params : N=#{N} K=#{K} P=#{P}"
+last = []
+last_data = 0
 (K..N).each do |i|
-  puts (P[0..(i-1)].max(K)).min
+  if i == K then
+    last = P[0..(i-1)].max(K)
+    #STDERR.puts "#{last}"
+  else 
+   if last_data < P[i-1]
+    last.delete(last_data)
+    last << P[i-1]
+   end
+  end
+   #STDERR.puts "last  = #{last}"
+  last_data = last.min
+  puts "#{last_data}"
 end
